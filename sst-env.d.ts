d47a5394562640d55ee96f4d9022e6049efb05af
Leonardo Petrucci <leonardo@petruc.ci>
2025-04-07 21:41:27 +0100

    Hopefully this works

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -2,8 +2,7 @@
 /* tslint:disable */
 /* eslint-disable */
 /* deno-fmt-ignore-file */
-import "sst"
-export {}
+
 declare module "sst" {
   export interface Resource {
     "GitHubFunction": {
@@ -17,3 +16,7 @@ declare module "sst" {
     }
   }
 }
+/// <reference path="sst-env.d.ts" />
+
+import "sst"
+export {}
